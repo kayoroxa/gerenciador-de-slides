b258c8b351c7ab2809cb1652e571daf36dbdce13
Caio <kayoroxa@gmail.com>
2022-01-11 22:20:40 -0300

    init

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    file: 'F_1',
+    img: 'https://drive.google.com/file/d/1MHO2VXvwlzmoee6avEVwIoiDyu7w4r3V/view?usp=sharing',
+    session: 'Pronun_1',
+  },
+  {
+    file: 'F_1',
+    img: 'https://drive.google.com/file/d/1Wj7fYjzK8dBdaJMH7Afelab6PFylCFb1/view?usp=sharing',
+    session: 'montar_frase_1',
+  },
+]
