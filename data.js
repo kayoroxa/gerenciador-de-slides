d44cb89d67875f5d50643eb9e84292560a6e6acf
Caio <kayoroxa@gmail.com>
2022-01-11 23:33:45 -0300

    .

--- data.js
+++ data.js
@@ -19,4 +19,14 @@ const data = [
     img: 'https://drive.google.com/file/d/1V3IroctBeYu0NO6gbLCQ5zyCDjCe9dHA/view?usp=sharing',
     session: 'exemple_1',
   },
+  {
+    file: 'F_1',
+    img: 'https://drive.google.com/file/d/1rB6Vgrv-0sxNCK7teNEEzNXtXscVQB-X/view?usp=sharing',
+    session: 'frase_pattern_2',
+  },
+  {
+    file: 'F_1',
+    img: '',
+    session: '',
+  },
 ]
